188b0f30143599c0c64dc270fb5adbf0eccf1eb5
Noemikiss <92308418+Noemikiss@users.noreply.github.com>
2026-07-28 20:46:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,15 +47,15 @@ function showTasks() {
         text.className = "task-text";
 
         let completeBtn = document.createElement("button");
-        completeBtn.textContent = "✓";
+        completeBtn.innerHTML = "&#10003;";
         completeBtn.className = "complete-btn";
 
         let deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "✗";
+        deleteBtn.innerHTML = "&#215;";
         deleteBtn.className = "delete-btn";
 
         let editBtn = document.createElement("button");
-        editBtn.textContent = "✎";
+        editBtn.innerHTML = "&#9998;";
         editBtn.className = "edit-btn";
 
         // COMPLETE
